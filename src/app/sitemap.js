8768ac912c1510e28e515d293f5cdaef258a0eaf
gitAji <89026824+gitAji@users.noreply.github.com>
2025-09-19 00:52:11 +0200

    v

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,42 +1,42 @@
 export default function sitemap() {
-  const baseUrl = 'https://www.aone.no';
+  const baseUrl = "https://www.aone.no";
   const pages = [
-    '/',
-    '/about',
-    '/client-login',
-    '/contact',
-    
-    '/references',
-    '/references/kids-learning-portal',
-    '/references/saray-steakhouse-kro',
-    '/references/clean-masters-renhold',
-    '/references/tulips',
-    '/references/qfs-accoutant',
-    '/references/rent-my-property-uk',
-    '/references/kids-learning-portal-development',
-    '/references/saray-beauty-parlour-website',
-    '/references/tulips-beauty-parlour-website',
-    '/services',
-    '/services/ai-automations',
-    '/services/branding',
-    '/services/digital-marketing',
-    '/services/photography',
-    '/services/search-engine-optimization',
-    '/services/ui-ux-design',
-    '/services/videography',
-    '/services/web-development',
+    "/",
+    "/about",
+    "/client-login",
+    "/contact",
+
+    "/references",
+    "/references/kids-learning-portal",
+    "/references/saray-steakhouse-kro",
+    "/references/clean-masters-renhold",
+    "/references/tulips",
+    "/references/qfs-accoutant",
+    "/references/rent-my-property-uk",
+    "/references/rent-cars",
+    "/references/shop-front",
+    "/references/trendifytools",
+    "/services",
+    "/services/ai-automations",
+    "/services/branding",
+    "/services/digital-marketing",
+    "/services/photography",
+    "/services/search-engine-optimization",
+    "/services/ui-ux-design",
+    "/services/videography",
+    "/services/web-development",
   ];
 
   const urls = pages.map((page) => ({
     url: `${baseUrl}${page}`,
     lastModified: new Date(),
-    changeFrequency: 'daily',
-    priority: page === '/' ? 1.0 : 0.8,
+    changeFrequency: "daily",
+    priority: page === "/" ? 1.0 : 0.8,
   }));
 
   urls.push({
-    url: 'https://blog.aone.no',
-    changeFrequency: 'daily',
+    url: "https://blog.aone.no",
+    changeFrequency: "daily",
     priority: 0.9,
   });
 
