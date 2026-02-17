e0736d3afde326c84816cd1d280c44abd13a035e
Ajee <89026824+gitAji@users.noreply.github.com>
2026-02-17 11:56:09 +0100

    mn

--- src/components/VoiceflowChat.js
+++ src/components/VoiceflowChat.js
@@ -3,30 +3,24 @@ import React from 'react';
 import Script from 'next/script';
 
 const VoiceflowChat = () => {
-    return (
-        <Script
-            id="voiceflow-chat"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-                __html: `
-          (function(d, t) {
-              var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
-              v.onload = function() {
-                window.voiceflow.chat.load({
-                  verify: { projectID: '699382016bc24417c151dcc6' },
-                  url: 'https://general-runtime.voiceflow.com',
-                  versionID: 'production',
-                  voice: {
-                    url: "https://runtime-api.voiceflow.com"
-                  }
-                });
-              }
-              v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
-          })(document, 'script');
-        `,
-            }}
-        />
-    );
+  return (
+    <Script
+      id="voiceflow-chat"
+      src="https://cdn.voiceflow.com/widget-next/bundle.mjs"
+      type="text/javascript"
+      strategy="afterInteractive"
+      onLoad={() => {
+        window.voiceflow.chat.load({
+          verify: { projectID: '699382016bc24417c151dcc6' },
+          url: 'https://general-runtime.voiceflow.com',
+          versionID: 'production',
+          voice: {
+            url: "https://runtime-api.voiceflow.com"
+          }
+        });
+      }}
+    />
+  );
 };
 
 export default VoiceflowChat;
